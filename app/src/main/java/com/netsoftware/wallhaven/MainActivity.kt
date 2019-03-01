06625a23a4c0baf8ae6baffd72b8fab32c1a6abc
Vlad Valkovoy <ascriiiii@gmail.com>
2019-03-01 16:52:40 +0200

    Initial commit

---
+++ app/src/main/java/com/netsoftware/wallhaven/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.netsoftware.wallhaven
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.netsoftware.wallhaven.databinding.MainActivityBinding
+import com.netsoftware.wallhaven.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+        binding.textView.text = "Some Text"
+    }
+}
